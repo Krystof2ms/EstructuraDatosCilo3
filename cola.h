37a50b7e88f9f7fbdc169b4703ad068ca6b9f5c4
KiaraMachaCeferino <60004930@continental.edu.pe>
2026-05-28 00:38:04 -0500

    cola-Mc

--- cola.h
+++ cola.h
@@ -21,3 +21,31 @@ struct Cola
     NodoCola *inicio;
     NodoCola *final;
 };
+
+void encolar(Cola*& head, int idProceso) {
+    NodoCola* nuevo_nodo = new NodoCola(idProceso);
+
+    if (head->final == NULL) {
+        head->inicio = nuevo_nodo;
+        head->final = nuevo_nodo;
+    } else {
+        head->final->siguiente = nuevo_nodo;
+        head->final = nuevo_nodo;
+    }
+}
+
+void desencolar(Cola*& head, int* idProceso) {
+    if (head->inicio == NULL) {
+        return;
+    }
+
+    NodoCola* temporal = head->inicio;
+    *idProceso = temporal->idProceso;
+    head->inicio = head->inicio->siguiente;
+
+    if (head->inicio == NULL) {
+        head->final = NULL;
+    }
+
+    delete temporal;
+}
