9e3fdf0cdcbb29639343bd9c8a84bc5d26ca2800
KiaraMachaCeferino <60004930@continental.edu.pe>
2026-05-27 23:48:28 -0500

    lista

---
+++ lista.h
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Proceso {
+    int id;
+    string nombre;
+    int prioridad;
+};
+
+struct Lista {
+    Proceso proceso;
+    Lista* siguiente;
+
+    Lista(Proceso proceso) {
+        siguiente = NULL;
+    }
+};
+
+void insertarInicio(Lista*& inicio, Proceso proceso) {
+    Lista* nuevoNodo = new Lista(proceso);
+    nuevoNodo->siguiente = inicio;
+    inicio = nuevoNodo;
+}
+
+void insertarFinal(Lista*& inicio, Proceso proceso) {
+    Lista* nuevoNodo = new Lista(proceso);
+    nuevoNodo->siguiente = NULL;
+
+    if (inicio == NULL) {
+        inicio = nuevoNodo;
+        return;
+    }
+
+    Lista* temp = inicio;
+    while (temp->siguiente != NULL) {
+        temp = temp->siguiente;
+    }
+    temp->siguiente = nuevoNodo;
+}
+
+void eliminarProceso(Lista*& inicio, int id) {
+    Lista* temp = inicio;
+    Lista* nodoAEliminar;
+
+    if (inicio == NULL) {
+        return;
+    }
+
+    if (inicio->proceso.id == id) {
+        inicio = inicio->siguiente;
+        delete temp;
+        return;
+    }
+
+    while (temp->siguiente != NULL && temp->siguiente->proceso.id != id) {
+        temp = temp->siguiente;
+    }
+
+    if (temp->siguiente != NULL) {
+        nodoAEliminar = temp->siguiente;
+        temp->siguiente = nodoAEliminar->siguiente;
+        delete nodoAEliminar;
+    }
+}
