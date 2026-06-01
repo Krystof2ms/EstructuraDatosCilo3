46e9469209719f99056dca4636390b5f57097697
AkemiTinocoRicra <61203454@continental.edu.pe>
2026-05-31 19:39:08 -0500

    Añadiendo importaciones

--- principal.cpp
+++ principal.cpp
@@ -1,3 +1,17 @@
+#include <iostream>
+#include <string>
+#include <locale.h>
+#include "pila.h"
+#include "lista.h"
+#include "cola.h"
+#include "validador.h"
+
+using namespace std;
+
+Cola *colaListos = new Cola();
+Lista *inicioLista = NULL;
+Pila *inicioPila = NULL;
+
 int main() {
     return 0;
 }
